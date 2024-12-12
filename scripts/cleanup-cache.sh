6bfdd19c1d31fb741feb87fcce075a7b8f04f0ee
Matthieu Dorier <mdorier@anl.gov>
2024-12-12 16:11:46 +0000

    more debugging

--- scripts/cleanup-cache.sh
+++ scripts/cleanup-cache.sh
@@ -20,7 +20,7 @@ list_packages() {
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer ${GITHUB_TOKEN}" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
-      "https://api.github.com/orgs/mochi-hpc/packages?package_type=container" > response.json
+      "https://api.github.com/orgs/mochi-hpc/packages?package_type=container&per_page=100&page=1" > response.json
     cat response.json 1>&2
 
 #    curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
