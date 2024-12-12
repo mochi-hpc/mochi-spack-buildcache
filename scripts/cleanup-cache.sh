8553ad816db41a56f0ea9b25e48161985c21daa3
Matthieu Dorier <mdorier@anl.gov>
2024-12-12 16:05:22 +0000

    debugging

--- scripts/cleanup-cache.sh
+++ scripts/cleanup-cache.sh
@@ -15,15 +15,23 @@ API_URL="https://api.github.com"
 
 # Function to fetch all packages
 list_packages() {
-    curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
-    -H "Accept: application/vnd.github+json" \
-    "${API_URL}/orgs/${REPO_OWNER}/packages?package_type=container" > response.json
-    echo "Response:"
+
+    curl -L \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      "https://api.github.com/orgs/mochi-hpc/packages?package_type=container" > response.json
     cat response.json 1>&2
-    echo "---"
-    curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
-        -H "Accept: application/vnd.github+json" \
-        "${API_URL}/orgs/${REPO_OWNER}/packages?package_type=container" | jq -r '.[].name'
+
+#    curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+#    -H "Accept: application/vnd.github+json" \
+#    "${API_URL}/orgs/${REPO_OWNER}/packages?package_type=container" > response.json
+#    echo "Response:"
+#    cat response.json 1>&2
+#    echo "---"
+#    curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+#        -H "Accept: application/vnd.github+json" \
+#        "${API_URL}/orgs/${REPO_OWNER}/packages?package_type=container" | jq -r '.[].name'
 }
 
 # Function to delete a package
